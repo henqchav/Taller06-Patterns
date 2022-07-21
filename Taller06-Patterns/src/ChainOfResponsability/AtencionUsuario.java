e3258c2cc67f1859f988f055cba30f2346eb71e3
henqchav <93171012+henqchav@users.noreply.github.com>
2022-07-20 22:39:00 -0500

    CoR

---
+++ Taller06-Patterns/src/ChainOfResponsability/AtencionUsuario.java
@@ -0,0 +1,11 @@
+package ChainOfResponsability;
+
+public class AtencionUsuario extends RequestChain{
+
+	public void nextRequest() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void verificaGarantia(){}
+
+}
---
+++ Taller06-Patterns/src/ChainOfResponsability/RequestChain.java
@@ -0,0 +1,6 @@
+package ChainOfResponsability;
+
+public abstract class RequestChain {
+
+	public abstract void nextRequest();
+}

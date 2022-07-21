bdb162890e67d70eae806e6f0e0d6d58516486e7
henqchav <93171012+henqchav@users.noreply.github.com>
2022-07-20 23:09:30 -0500

    CoR

---
+++ Taller06-Patterns/src/ChainOfResponsibility/DepartamentoTecnico.java
@@ -0,0 +1,14 @@
+package ChainOfResponsability;
+
+public class DepartamentoTecnico extends RequestChain{
+
+	public void nextRequest() {
+		// TODO Auto-generated method stub
+		
+	}
+	public boolean garantiaCubierta(){
+		return true;
+	}
+
+}
+
---
+++ Taller06-Patterns/src/ChainOfResponsibility/JefeInventario.java
@@ -0,0 +1,14 @@
+package ChainOfResponsability;
+
+public class JefeInventario extends RequestChain{
+
+	public void nextRequest() {
+		// TODO Auto-generated method stub
+		
+	}
+	public boolean existenciaBodega(){
+		return true;
+	}
+
+}
+
